f6752db876c9439ce887614a10fe187d6917457c
Max Banin <banin@ukr.net>
2026-05-31 11:04:41 +0300

    2

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+  categoriesList: document.querySelector('.categories'),
+  productsList: document.querySelector('.products'),
+  searchForm: document.querySelector('.search-form'),
+  searchInput: document.querySelector('.search-form__input'),
+  clearBtn: document.querySelector('.search-form__btn-clear'),
+};
